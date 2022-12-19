021f94404d9ffc048c8c40a2623f7f97dd63bad5
iuriimqa <120679092+iuriimqa@users.noreply.github.com>
2022-12-19 18:57:05 +0200

    changes in project

--- untitled/src/HellowWorld.java
+++
@@ -1,7 +0,0 @@
-import org.w3c.dom.ls.LSOutput;
-
-import javax.print.attribute.standard.PrinterLocation;
-
-public class HellowWorld { PrinterLocation  public static void main(String[] args) {
-    }
-}
